cfd2be1b386da02edd8b0a41d9abbcc459c35687
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2022-03-31 18:27:47 -0300

    Update main.cpp

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
-using std::cin;
-using std::cout;
+/* using std::cin; */
+/* using std::cout; */
+using namespace std;
 
 const int SIZE = 5; // input size.
 
 int main(void)
 {
     // TODO: Adicione aqui seu código.
+    int valores[5];
+
+    cin>>valores[0];
+    cin>>valores[1];
+    cin>>valores[2];
+    cin>>valores[3];
+    cin>>valores[4];
+
+    int numeros_negativos = 0;
+    for(int i=0;i<5;i++){
+        if(valores[i] < 0)
+            numeros_negativos += 1;
+    }
+
+    cout << numeros_negativos << endl;
 
     return 0;
 }
