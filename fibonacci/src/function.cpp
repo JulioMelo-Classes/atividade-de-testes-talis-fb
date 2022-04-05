1f1aec00f8d38bc0e90bedd6458c1774b98066f9
Talison Fabio <talisonf11@gmail.com>
2022-04-05 13:33:20 -0300

    Aprimoramento Atividade Fibonacci

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -3,17 +3,12 @@
 
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
-    /* vector<unsigned int> vazio = {}; */
-    if( n <= 1 )
-        return {};
+    if( n <= 1 ) return {};
 
     vector<unsigned int> valores = { 1, 1 };
-
-    for(int i=2; i < n; i++){
-        unsigned int valor_atual =  (valores[i-1] + valores[i-2]);
-        if( valor_atual >= n ){
-            break;
-        }
+    while( true ){
+        unsigned int valor_atual =  valores.end()[-1] + valores.end()[-2];
+        if( valor_atual >= n ) break;
         valores.push_back({ valor_atual });
     }
 
